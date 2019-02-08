96ba70a4df93bfd71be53bd814075efc0749b848
Nosolution <32201220+Nosolution@users.noreply.github.com>
2019-02-08 23:20:32 +0800

    feat: 引入验证码验证功能

--- src/main/java/backend/util/captchaUtil/Captcha.java
+++ src/main/java/backend/util/captchaUtil/Captcha.java
@@ -1,32 +1,41 @@
 package backend.util.captchaUtil;
 
+import com.google.code.kaptcha.impl.DefaultKaptcha;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.OutputStream;
 
 public class Captcha {
-    private int width;
-    private int height;
-    private String code;
-    private BufferedImage buffImg = null;
+    @Autowired
+    DefaultKaptcha defaultKaptcha;
 
-    public Captcha() {
+    String text;
+    BufferedImage image;
+
+    public Captcha(){
+        getCode();
     }
 
-    public Captcha(int width, int height, String code) {
-        this.width = width;
-        this.height = height;
-        this.code = code;
+    public String getCode(){
+        text=defaultKaptcha.createText();
+        image = defaultKaptcha.createImage(text);
+        return text;
     }
 
-    public String getCode() {
-        return this.code;
-    }
+//    public BufferedImage getCaptchaImage(String text){
+//        if(image!=null){
+//            return image;
+//        }else {
+//
+//        }
+//    }
 
-    public void write(OutputStream sos) throws IOException {
-        ImageIO.write(buffImg, "png", sos);
-        sos.close();
+    public void write(OutputStream os) throws IOException{
+        ImageIO.write(image, "png", os);
+        os.close();
     }
 
 }
