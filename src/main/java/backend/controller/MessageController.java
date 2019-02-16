69fbeb311266086b40a91e86dccb03149e0e5a41
Nosolution <32201220+Nosolution@users.noreply.github.com>
2019-02-16 14:33:32 +0800

    暂存修改

--- src/main/java/backend/controller/MessageController.java
+++ src/main/java/backend/controller/MessageController.java
@@ -1,12 +1,18 @@
 package backend.controller;
 
 
+import backend.dao.impl.HibernateDao;
 import backend.parameter.SetMessage.MessageParam;
 import backend.response.Message.Message;
 import backend.service.MessageService;
+import backend.util.token.JwtToken;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
 
 @CrossOrigin
 @RestController
@@ -15,11 +21,27 @@ public class MessageController {
     @Autowired
     MessageService service;
 
+    @Autowired
+    JwtToken jwtToken;
+
+    @Value("${tokenHeader}")
+    String header;
+
     @RequestMapping(value = "/set",
             method = RequestMethod.POST,
             consumes = {"application/json", "application/xml"})
     public void setMessage(@RequestBody MessageParam param)throws Exception{
         service.updateState(param.getMessage());
+    }
+
+    @RequestMapping(value = "/confirmation",
+        method = RequestMethod.POST)
+    public void confirmSecondTestAttendance(@RequestBody Map<String, Boolean> param, HttpServletRequest request){
+        if(!param.containsKey("willAttend")) return;
+        JwtToken jwtToken = new JwtToken();
+        String token = request.getHeader(header).substring(7);
+        String email = jwtToken.getUsernameFromToken(token);
+        service.confirmSecondTestAttendance(email, param.get("willAttend"));
     }
 
 }
--- src/main/java/backend/service/MessageService.java
+++ src/main/java/backend/service/MessageService.java
@@ -2,6 +2,7 @@ package backend.service;
 
 import backend.dao.impl.HibernateDao;
 import backend.entity.Administer;
+import backend.entity.AssessmentResult;
 import backend.entity.Student;
 import backend.enums.StudentState;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,5 +25,13 @@ public class MessageService {
         HibernateDao<Administer> dao = new HibernateDao<>(new Administer());
         Administer administer=dao.getAllObjects().get(0);
         return administer.getMessage();
+    }
+
+    public void confirmSecondTestAttendance(String email, boolean willing){
+        HibernateDao<AssessmentResult> dao = new HibernateDao<>(new AssessmentResult());
+        String sql = "SELECT r FROM AssessmentResult.r WHERE r.email = \'"+email+"\'";
+        AssessmentResult r = dao.executeQuerySql(sql).get(0);
+        r.setAttendSecondTest(willing);
+        dao.update(r);
     }
 }
