7f2e0eb6212550f70bac57f55d9283c84c7c3d8b
Nosolution <32201220+Nosolution@users.noreply.github.com>
2019-02-24 18:19:02 +0800

    附下

--- src/main/java/backend/config/KaptchaConfig.java
+++ src/main/java/backend/config/KaptchaConfig.java
@@ -16,10 +16,10 @@ public class KaptchaConfig {
         Properties properties = new Properties();
         properties.setProperty("kaptcha.border", "yes");
         properties.setProperty("kaptcha.border.color", "105,179,90");
-        properties.setProperty("kaptcha.textproducer.font.color", "blue");
-        properties.setProperty("kaptcha.image.width", "110");
-        properties.setProperty("kaptcha.image.height", "40");
-        properties.setProperty("kaptcha.textproducer.font.size", "30");
+        properties.setProperty("kaptcha.textproducer.font.color", "0,60,255");
+        properties.setProperty("kaptcha.image.width", "160");
+        properties.setProperty("kaptcha.image.height", "60");
+        properties.setProperty("kaptcha.textproducer.font.size", "45");
         properties.setProperty("kaptcha.session.key", "code");
         properties.setProperty("kaptcha.textproducer.char.length", "4");
         properties.setProperty("kaptcha.textproducer.font.names", "宋体,楷体,微软雅黑");
