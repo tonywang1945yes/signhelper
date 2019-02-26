1584964c92ed761de002436381f369b2bf267756
Disclover <171250639@smail.nju.edu.cn>
2019-02-26 15:56:08 +0800

    plus

--- src/main/java/backend/controller/MajorController.java
+++ src/main/java/backend/controller/MajorController.java
@@ -3,15 +3,13 @@ package backend.controller;
 
 import backend.parameter.setMajor.SetMajorParameter;
 import backend.service.MajorService;
-import backend.service.SetDdLService;
-import backend.service.SetMajorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin
 @RestController()
 @RequestMapping(value = "/majorSetting")
-public class SetMajorController {
+public class MajorController {
 
     @Autowired
     MajorService majorService;
