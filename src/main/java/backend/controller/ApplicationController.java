4b0950fb54117e332dc7fcf61a4cca9b8deaf81c
Nosolution <32201220+Nosolution@users.noreply.github.com>
2019-03-02 11:38:28 +0800

    refactor: 将controller的逻辑移至service里

--- src/main/java/backend/controller/ApplicationController.java
+++ src/main/java/backend/controller/ApplicationController.java
@@ -5,17 +5,14 @@ import backend.response.BasicResponse;
 import backend.response.application.ApplFormResponse;
 import backend.service.ApplicationService;
 import backend.util.token.JwtToken;
-import com.hankcs.hanlp.HanLP;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import javax.servlet.http.HttpServletRequest;
-import java.io.File;
-import java.io.IOException;
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
 @CrossOrigin
 @RestController
@@ -57,84 +54,32 @@ public class ApplicationController {
 //            return new BasicResponse(false, "超过提交时间");
         String type = multiRequest.getParameter("type");
         String email = getIdFromRequest(multiRequest);
-        String studentName = service.getStudentName(email);
-        File target = new File(dest + "/" + studentName + "-" + email + "/" + type);
-        if (!target.exists())
-            target.mkdirs();
-        else {//如果目录里已有文件则先清空，仅局限于文件而无法删除子文件夹
-            String[] items = target.list();
-            for (String item : items) {
-                File fullPath = new File(target, item);
-                fullPath.delete();
-            }
-        }
+        boolean res = service.sendAttachment(dest, email, type, multiRequest.getFiles("file"));//需要键名为file
+        return new BasicResponse(res, res ? "" : "上传失败");
 
-        List<MultipartFile> files = multiRequest.getFiles("file");//这里似乎需要某部分的名字为file？
-        try {
-            for (MultipartFile file : files)
-                file.transferTo(new File(target, file.getOriginalFilename()));
-        } catch (IOException e) {
-            e.printStackTrace();
-            return new BasicResponse(false, "上传失败");
-        }
-        return new BasicResponse(true, "");
     }
 
     @RequestMapping(value = "/attachment_check", method = RequestMethod.POST)
     public Map<String, Boolean> hasUploadedAttachment(@RequestBody Map<String, String[]> param, HttpServletRequest request, @Value("${savingPath}") String dest) {
         Map<String, Boolean> result = new HashMap<>();
-        result.put("hasUploaded", true);
-
-        String email = getIdFromRequest(request);
-        String studentName = service.getStudentName(email);
-        File target = new File(dest + "/" + studentName + "-" + email);
-
-        if (!target.exists()) {
-            result.put("hasUploaded", false);
-            return result;
-        }
-        for (String folder : param.get("types")) {
-            File fullPath = new File(target, folder);
-            if (!fullPath.exists() || fullPath.list().length == 0) {
-                result.put("hasUploaded", false);
-                return result;
-            }
-        }
+        result.put("hasUploaded", service.hasUploadedAttachment(dest, getIdFromRequest(request), param.get("types")));
         return result;
     }
 
     @RequestMapping(value = "/attachment_names", method = RequestMethod.GET)
     public String[] getFileNames(HttpServletRequest request, @Value("${savingPath}") String dest) {
         String email = getIdFromRequest(request);
-        String studentName = service.getStudentName(email);
-        File target = new File(dest + studentName + "-" + email);
-        if (!target.exists())
-            return null;
-        List<String> result = new ArrayList<>();
-        String[] folders = target.list();
-        for (String folder : folders)
-            result.addAll(Arrays.asList(new File(target, folder).list()));
-        String[] names = new String[result.size()];
-        return result.toArray(names);
+        return service.getFileNames(dest, email);
     }
 
     @RequestMapping(value = "/simplify_api",
             method = RequestMethod.POST)
     public Map<String, String> simplifyChars(@RequestBody Map<String, String> param) {
         Map<String, String> res = new HashMap<>();
-        if (!param.containsKey("raw")) {
-            res.put("simplifiedChars", "");
-            return res;
-        }
-        StringBuilder raw = new StringBuilder(param.get("raw"));
-        StringBuilder simplified = new StringBuilder();
-        for (int i = 0; i < raw.length(); i++) {
-            //因担心发生词语转换而拆开字符串分开转换，性能有待研究
-            simplified.append(HanLP.convertToSimplifiedChinese(raw.substring(i, i + 1)));
-        }
-        res.put("simplifiedChars", simplified.toString());
+        res.put("simplifiedChars", param.containsKey("raw") ? service.simplifyChars(param.get("raw")) : "");
         return res;
     }
+
 
     private String getIdFromRequest(HttpServletRequest request) {
         String token = request.getHeader(tokenHeader).substring(7);
--- src/main/java/backend/service/ApplicationService.java
+++ src/main/java/backend/service/ApplicationService.java
@@ -3,11 +3,18 @@ package backend.service;
 import backend.dao.service.*;
 import backend.entity.application.ApplForm;
 import backend.parameter.application.ApplFormParameter;
+import com.hankcs.hanlp.HanLP;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Calendar;
+import java.util.List;
 
 @Service
 public class ApplicationService {
@@ -42,12 +49,68 @@ public class ApplicationService {
         return true;
     }
 
+    public boolean sendAttachment(String dest, String email, String type, List<MultipartFile> files) {
+        File target = new File(getRootPath(dest, email), type);
+        if (!target.exists())
+            target.mkdirs();
+        else {//如果目录里已有文件则先清空，仅局限于文件而无法删除子文件夹
+            String[] items = target.list();
+            for (String item : items) {
+                File fullPath = new File(target, item);
+                fullPath.delete();
+            }
+        }
+        try {
+            for (MultipartFile file : files)
+                file.transferTo(new File(target, file.getOriginalFilename()));
+        } catch (IOException e) {
+            e.printStackTrace();
+            for (String item : target.list())
+                new File(target, item).delete();
+            target.delete();
+            return false;
+        }
+        return true;
+    }
+
+    public boolean hasUploadedAttachment(String dest, String email, String[] types) {
+        File rootPath = getRootPath(dest, email);
+        if (!rootPath.exists())
+            return false;
+        for (String folder : types) {
+            File fullPath = new File(rootPath, folder);
+            if (!fullPath.exists() || fullPath.list().length == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public String[] getFileNames(String dest, String email) {
+        File rootPath = getRootPath(dest, email);
+        if (!rootPath.exists())
+            return null;
+
+        List<String> result = new ArrayList<>();
+        String[] folders = rootPath.list();
+        for (String folder : folders)
+            result.addAll(Arrays.asList(new File(rootPath, folder).list()));
+        String[] names = new String[result.size()];
+        return result.toArray(names);
+    }
+
     public ApplForm getApplicationForm(String studentId) {
         return applFormRepo.findByStudentId(studentId).get(0);
     }
 
-    public String getStudentName(String email) {
-        return studentRepo.getOne(email).getName();
+    public String simplifyChars(String origin) {
+        StringBuilder raw = new StringBuilder(origin);
+        StringBuilder simplified = new StringBuilder();
+        for (int i = 0; i < raw.length(); i++) {
+            //因担心发生词语转换而拆开字符串分开转换，性能有待研究
+            simplified.append(HanLP.convertToSimplifiedChinese(raw.substring(i, i + 1)));
+        }
+        return simplified.toString();
     }
 
     public boolean beforeDDL() {
@@ -55,5 +118,15 @@ public class ApplicationService {
         Calendar ddl = administerRepo.findAll().get(0).getDdl();
         return now.before(ddl);
     }
+
+    private String getStudentName(String email) {
+        return studentRepo.getOne(email).getName();
+    }
+
+    private File getRootPath(String dest, String email) {
+        String studentName = getStudentName(email);
+        return new File(dest + "/" + studentName + "-" + email);
+    }
+
 
 }
