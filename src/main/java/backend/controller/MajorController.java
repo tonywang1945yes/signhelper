d2bc3d668064cd2c16c588bb25421b705f7f7c4e
Disclover <171250639@smail.nju.edu.cn>
2019-03-06 10:16:16 +0800

    add delete major impl

--- src/main/java/backend/controller/MajorController.java
+++ src/main/java/backend/controller/MajorController.java
@@ -3,6 +3,7 @@ package backend.controller;
 
 import backend.entity.Major;
 import backend.parameter.setMajor.SetMajorParameter;
+import backend.response.BasicResponse;
 import backend.service.MajorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -18,10 +19,30 @@ public class MajorController {
     MajorService majorService;
 
 
-    @PostMapping(value = "/majorSetting")
-    public void addMajor(@RequestBody SetMajorParameter parameter){
-        majorService.add(parameter.getName(), parameter.getStuNum(), parameter.getAcceptArt(), parameter.getTime(), parameter.getCollege(), parameter.getPrice(), parameter.getComment());
+    @PostMapping(value = "/majorAdding")
+    public BasicResponse addMajor(@RequestBody SetMajorParameter parameter){
+        Boolean res = majorService.add(parameter.getName(), parameter.getStuNum(), parameter.getAcceptArt(), parameter.getTime(), parameter.getCollege(), parameter.getPrice(), parameter.getComment());
+        if (res){
+            return new BasicResponse(true, "Succeed.");
+        }
+        else {
+            return new BasicResponse(false, "Fail to add major.");
+        }
     }
+
+    @PostMapping(value = "/majorDeleting")
+    public BasicResponse deleteMajor(@RequestBody String name){
+        Boolean res = majorService.delete(name);
+        if (res){
+            return new BasicResponse(true, "Succeed.");
+        }
+        else {
+            return new BasicResponse(false, "Fail to delete major.");
+        }
+    }
+
+
+
 
     @GetMapping(value = "/majorGetting")
     public Major[] getMajors(){
--- src/main/java/backend/service/MajorService.java
+++ src/main/java/backend/service/MajorService.java
@@ -3,6 +3,7 @@ package backend.service;
 
 import backend.dao.service.MajorRepository;
 import backend.entity.Major;
+import backend.response.BasicResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,9 +16,20 @@ public class MajorService {
     @Autowired
     MajorRepository majorRepo;
 
-    public void add(String name, Integer stuNum, Boolean acceptArt, Integer time, String college, Integer price, String comment){
+    public Boolean add(String name, Integer stuNum, Boolean acceptArt, Integer time, String college, Integer price, String comment){
         majorRepo.save(new Major(name, stuNum, acceptArt, time, college, price, comment));
+        return isExist(name);
     }
+
+    public Boolean delete(String name){
+        majorRepo.delete(majorRepo.findByName(name));
+        return !isExist(name);
+    }
+
+    private Boolean isExist(String name){
+        return majorRepo.findByName(name) != null;
+    }
+
 
     public List<Major> getMajors(){
         return majorRepo.findAll();
